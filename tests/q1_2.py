6e612578a2846905d727ad1be323e5e28558dbe6
aarapi <albiarapi1@gmail.com>
2022-09-22 19:29:12 +0200

    First commit

---
+++ tests/q1_2.py
@@ -0,0 +1,23 @@
+OK_FORMAT = True
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> str(mark_hurd_pay_string)
+          '$53.25 '
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ tests/q2_1.py
@@ -0,0 +1,23 @@
+OK_FORMAT = True
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> to_percentage(.35) == 35.0
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
